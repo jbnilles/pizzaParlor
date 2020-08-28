934dcedb7fee2ec9ac31eb12fa6330e635ac2d8c
jbnilles <jbnilles24@gmail.com>
2020-08-28 08:57:57 -0700

    add initial mehthods to objects

--- js/scripts.js
+++ js/scripts.js
@@ -16,32 +16,109 @@ function Order () {
 function Order (name) {
   let items = [];
   let name = name;
-  let subtotal;
-  let total;
+  let subtotal = 0;
+  let tax = 0
+  let total = 0;
+}
+Order.prototype.addItem = function (item) {
+  this.items.push(item);
+  this.subtotal += item.cost;
+  return this.subtotal;
+}
+Order.prototype.calculateTotal = function () {
+this.tax = this.subtotal * .09;
+this.total = this.subtotal += this.tax;
+return this.total;
+}
+Order.prototype.getTax = function () {
+  return this.tax;
+}
+Order.prototype.getSubtotal = function () {
+  return this.subtotal;
+}
+Order.prototype.getTotal = function () {
+  return this.total;
 }
 
 function Pizza () {
   let size;
   let toppings = [];
-  let cost;
+  let cost = 0;
 }
 
-Pizza.prototype.calculateCost () {
-  switch (this.) {
-    case value:
-      
+Pizza.prototype.calculateCost = function () {
+  switch (this.size) {
+    case 'personal':
+      this.cost += 5;
       break;
-  
+
+    case 'small':
+      this.cost += 8;
+      break;
+
+    case 'medium':
+      this.cost += 10;
+      break;
+
+    case 'large':
+    this.cost += 12;
+    break;
+
+    case 'x-large':
+    this.cost += 15;
+    break;
+
     default:
       break;
   }
+  if(this.toppings.length > 2){
+    this.cost += this.toppings.length - 2;
+  }
+  return this.cost;
+}
+Pizza.prototype.addTopping = function (topping) {
+  this.toppings.push(topping);
+}
+Pizza.prototype.setSize = function (size) {
+  this.size = size;
+}
+Pizza.prototype.getCost = function () {
+  return this.cost;
 }
 
 
 function Drink () {
   let size;
   let flavor;
-  let style;
+  let cost = 0;
+
+}
+Drink.prototype.setSize = function (size) {
+  this.size = size;
+}
+Drink.prototype.setFlavor = function (flavor) {
+  this.flavor = flavor;
+}
+Drink.prototype.calculateCost = function () {
+  switch (this.size) {
+    case 'small':
+      this.cost = 1
+      break;
+
+    case 'medium':
+      this.cost = 1.50
+      break;
+
+    case 'small':
+      this.cost = 2
+      break;
+
+    default:
+      break;
+  }
+}
+Drink.prototype.getCost = function () {
+  return this.cost;
 }
 
 
