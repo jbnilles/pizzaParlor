5135edb465f437b3591e689b4097f4ab1d84cc99
jbnilles <jbnilles24@gmail.com>
2020-08-28 08:34:17 -0700

    add initial objects to js

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+function store () {
+  let previousOrders = [];
+  let toppings = ['peparoni', 'bacon', 'extra cheese', 'mushrooms', 'onions', 'olive', 'pineapple'];
+  let drinkFlavors = ['coke','diet coke', 'rootbeer', 'lemonade'];
+  let pizzaSizes = ['personal', 'small', 'medium', 'large', 'x-large'];
+  let drinkSizes = ['small', 'medium', 'large'];
+}
+
+
+function Order () {
+  let items = [];
+  let name = '';
+  let subtotal;
+  let total;
+}
+function Order (name) {
+  let items = [];
+  let name = name;
+  let subtotal;
+  let total;
+}
+
+function Pizza () {
+  let size;
+  let toppings = [];
+  let cost;
+}
+
+Pizza.prototype.calculateCost () {
+  switch (this.) {
+    case value:
+      
+      break;
+  
+    default:
+      break;
+  }
+}
+
+
+function Drink () {
+  let size;
+  let flavor;
+  let style;
+}
+
+
+
+$().ready(function () {
+
+
+
+});
